97f462da4b21ced2d559ab92f24be3255a0f5398
ykrantz <ykrantz@gmail.com>
2022-06-09 18:16:10 +0300

    fix delete from favorites

--- BL/userFavoritesLogic.js
+++ BL/userFavoritesLogic.js
@@ -29,6 +29,64 @@ const getAllFavoriteCitiesOfUser = async (userDetails) => {
   }
 };
 
+const deleteCityToUserFavorite = async (userDetails, cityName) => {
+  // console.log("ADDDD");
+  // console.log({ userDetails }, { cityName }, 44);
+  const userId = userDetails?._id;
+  const cityDetails = await city.readOne({ name: cityName.toLowerCase() });
+  const city_id = cityDetails._id;
+  console.log(city_id, 16);
+
+  if (city_id) {
+    //   if city exist on DB, will delete city to favorite:
+    const existUserCityFavorites = await userFavorites.readOne({
+      user: userId,
+    });
+    // console.log({ existUserCityFavorites });
+    let newUserCityFavorites = {};
+    if (existUserCityFavorites) {
+      // if favortie for user  exist=> delete cityt
+      const cityFavoriteExist = await checkIfCityAlreadyExistInFavorite(
+        userId,
+        city_id
+      );
+      // console.log({ cityFavoriteExist }, 14);
+      if (cityFavoriteExist) {
+        // console.log(existUserCityFavorites._id, 13);
+        newUserCityFavorites = await userFavorites.update(
+          existUserCityFavorites._id,
+          { $pull: { favoriteCities: city_id } }
+        );
+        console.log(city_id, 15);
+        console.log("deleted city from favorite");
+      } else {
+        // if city was't in favorites , won't delete it
+        return new Respond(401, "", "city was't in favorite  list");
+      }
+    } else {
+      console.log("user does't have favorite ");
+      return new Respond(401, "", "user does't have favorite");
+    }
+
+    // const newUserCityFavorites = await userFavorites.updateByFilter(
+    //   {
+    //     user: userId,
+    //   },
+    //   { favoriteCities: city_id }
+    //   //   { $push: { favoriteCities: city_id } }
+    // );
+    const newUserCityFavoritesPopulate = await userFavorites.readOneAndPopulate(
+      { _id: newUserCityFavorites._id },
+      "",
+      "favoriteCities"
+    );
+    return new Respond(200, newUserCityFavoritesPopulate);
+  } else {
+    // return { code: 404, data: "no cities wasn't found" };
+    return new Respond(404, "", "city wan't found and not deleted");
+  }
+};
+
 const addCityToUserFavorite = async (userDetails, cityName) => {
   // console.log("ADDDD");
   // console.log({ userDetails }, { cityName });
@@ -41,7 +99,7 @@ const addCityToUserFavorite = async (userDetails, cityName) => {
     const existUserCityFavorites = await userFavorites.readOne({
       user: userId,
     });
-    console.log({ existUserCityFavorites });
+    // console.log({ existUserCityFavorites });
     let newUserCityFavorites = {};
     if (!existUserCityFavorites) {
       // if favortie for user doesn;t exist create favorite
@@ -57,7 +115,7 @@ const addCityToUserFavorite = async (userDetails, cityName) => {
         userId,
         city_id
       );
-      console.log({ cityFavoriteExist });
+      // console.log({ cityFavoriteExist });
       if (!cityFavoriteExist) {
         newUserCityFavorites = await userFavorites.update(
           existUserCityFavorites._id,
@@ -90,16 +148,17 @@ const addCityToUserFavorite = async (userDetails, cityName) => {
 
 const checkIfCityAlreadyExistInFavorite = async (userId, city_Id) => {
   // TODO: why if city doesnr exist i get message of exist
-  console.log({ userId }, { city_Id }, "444");
+  // console.log({ userId }, { city_Id }, "444");
   const cityAlreadyInFavorite = await userFavorites.readOne({
     user: userId,
     favoriteCities: { $in: city_Id },
   });
-  console.log({ cityAlreadyInFavorite });
+  // console.log({ cityAlreadyInFavorite });
   return cityAlreadyInFavorite ? true : false;
 };
 module.exports = {
   //   getAllFavoriteCities,
   getAllFavoriteCitiesOfUser,
   addCityToUserFavorite,
+  deleteCityToUserFavorite,
 };
